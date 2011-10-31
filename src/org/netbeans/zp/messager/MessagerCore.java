d05c5a11c493d46cec9fa55abc1731852a3b91be
Bartek Bułat <bartek.bulat@gmail.com>
2011-10-31 11:14:50 +0100

    Utworzenie projektu, dodanie biblioteki smack. Podstawowy .gitignore.

---
+++ src/org/netbeans/zp/messager/MessagerCore.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.netbeans.zp.messager;
+
+/**
+ *
+ * @author Bartłomiej Bułat <bartek.bulat at gmail.com>
+ */
+public class MessagerCore {
+  
+}
